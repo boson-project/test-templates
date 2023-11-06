a8cc27f8301f11a7764860daa696d2a3d68784c7
Jefferson Ramos <jeramos@redhat.com>
2023-11-06 14:00:33 -0300

    changing func volumes test template to also write simple content to files (#8)

--- go/volumes/handle.go
+++ go/volumes/handle.go
@@ -1,27 +1,38 @@
 package function
+
 /*
-This function template returns on body the content of a file on the server
+This function template read and (optionally) write the content of a file on the server
 The template is meant to be used in by `func config volumes` e2e test
 */
 import (
 	"context"
 	"fmt"
-	"io/ioutil"
-	"os"
 	"net/http"
-	"strings"
+	"os"
 )
 
 func Handle(ctx context.Context, res http.ResponseWriter, req *http.Request) {
 	res.Header().Add("Content-Type", "text/plain")
 
 	// v=/test/volume-config/myconfig
-	q := strings.Split(req.URL.RawQuery, "=")
-	action := q[0]
-	path := q[1]
+	// w=hello
+	path := req.URL.Query().Get("v")
+	content := req.URL.Query().Get("w")
 
-	if action == "v" {
-		b, err := ioutil.ReadFile(path)
+	if path != "" {
+		if content != "" {
+			f, err := os.Create(path)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "error creating file: %v\n", err)
+			} else {
+				defer f.Close()
+				err = os.WriteFile(path, []byte(content), 0644)
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "error writing file: %v\n", err)
+				}
+			}
+		}
+		b, err := os.ReadFile(path)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "error reading file: %v", err)
 		}
